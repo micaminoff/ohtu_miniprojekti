381790f19aef9f927d9f124b1323969f2e1e3b8c
Michael Aminoff <michael@noviaria.com>
2017-11-21 10:42:04 +0200

    Added successful search tests

--- src/test/java/ohtu/Stepdefs.java
+++ src/test/java/ohtu/Stepdefs.java
@@ -1,5 +1,6 @@
 package ohtu;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -18,7 +19,7 @@ public class Stepdefs {
     BookDao bdao = new InMemoryBookDao();
     SuggestionService sugg = new SuggestionService(bdao);
     List<String> inputLines = new ArrayList<>();
-    
+   
     @Given("^command add is selected$")
     public void command_add_selected() throws Throwable {
         inputLines.add("add");
@@ -29,9 +30,44 @@ public class Stepdefs {
         inputLines.add("list");
     }
     
+    @Given("^command find is selected$")
+    public void command_find_selected() throws Throwable {
+        inputLines.add("find");
+    }
+    
+    @Given("^command title is selected$")
+    public void command_title_selected() throws Throwable {
+        inputLines.add("title");
+    }
+    
+    @Given("^command creator is selected$")
+    public void command_creator_selected() throws Throwable {
+        inputLines.add("creator");
+    }
+    
+    @Given("^command description is selected$")
+    public void command_description_selected() throws Throwable {
+        inputLines.add("description");
+    }
+    
+    @Given("^command isbn is selected$")
+    public void command_isbn_selected() throws Throwable {
+        inputLines.add("isbn");
+    }
+    
     @When("^command book is selected$")
     public void command_book_selected() throws Throwable {
         inputLines.add("book");
+    }
+    
+    
+    @When("^search term \"([^\"]*)\" is entered")
+    public void term_entered(String term) throws Throwable {
+        inputLines.add(term);
+        io = new StubIO(inputLines); 
+        app = new App(io, sugg);
+        app.run();
+        
     }
     
     @When("^author \"([^\"]*)\" and title \"([^\"]*)\" and description \"([^\"]*)\" and ISBN \"([^\"]*)\" are entered$")
@@ -49,6 +85,12 @@ public class Stepdefs {
     @Then("^message \"([^\"]*)\" is displayed$")
     public void message_is_displayed(String expectedOutput) throws Throwable {
         assertTrue(io.getPrints().contains(expectedOutput));
+    }
+    
+    @Then("^book is found$")
+    public void book_is_found() throws Throwable {
+        assertTrue(io.getPrints().contains("Author: Robert Martin\nTitle: Clean Code: A Handbook of Agile Software Craftsmanship\nDescription: Good book\nISBN: 978-951-98548-9-2\n"));
+        
     }
 
 }
--- src/test/resources/ohtu/search_suggestions.feature
+++ src/test/resources/ohtu/search_suggestions.feature
@@ -1,9 +1,26 @@
 Feature: User can search for suggestions
 
-	Scenario: search suggestions by book's title
-		Given suggestions exist
-		When  user selects option "Search by title of book: "
-		Then  user is asked for a title of a book
-		Then  user gives title "Book1"
-		Then  user is given a list of suggestions related to book with title "Book1"
-		
+# Successful searches
+    Scenario: search suggestions by book's title
+	Given command find is selected
+	Given command title is selected
+	When search term "Clean Code: A Handbook of Agile Software Craftsmanship" is entered
+	Then book is found
+
+    Scenario: search suggestions by author
+        Given command find is selected
+	Given command creator is selected
+	When search term "Robert Martin" is entered
+	Then book is found
+
+    Scenario: search suggestions by description
+        Given command find is selected
+	Given command description is selected
+	When search term "Good book" is entered
+	Then book is found
+
+    Scenario: search suggestions by ISBN
+        Given command find is selected
+	Given command isbn is selected
+	When search term "978-951-98548-9-2" is entered
+	Then book is found
