1e91706d5010c2de23ab863592dfca568e168ba5
otsepp <tseppanen@protonmail.com>
2017-11-25 22:01:54 +0200

    podcast tyyppi

---
+++ src/main/java/ohtu/data_access/PodcastDao.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu.data_access;
+
+import java.util.List;
+import ohtu.domain.Podcast;
+
+/**
+ *
+ * @author otsepp
+ */
+public interface PodcastDao {
+    
+    List<Podcast> listAll();
+    List<Podcast> findByTitle(String podcast);
+    List<Podcast> findByCreator(String podcast);
+    Podcast findByUrl(String url);
+    List<Podcast> findByPodcastName(String podcastName);
+    
+}
---
+++ src/main/java/ohtu/domain/Podcast.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu.domain;
+
+/**
+ *
+ * @author otsepp
+ */
+public class Podcast implements Suggestable {
+    private String title;
+    private String creator; //vapaaeht
+    private String url;
+    private String podcastName;
+    
+    public Podcast(String title, String creator, String url, String podcastName) {
+        this.title = title;
+        this.creator = creator;
+        this.url = url;
+        this.podcastName = podcastName; 
+    }
+    
+    @Override
+    public String getTitle() {
+        return title;
+    }
+    
+    @Override
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getCreator() {
+        return creator;
+    }
+    
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+    
+    public String getUrl() {
+        return url;
+    }
+    
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+    public String getPodcastName() {
+        return podcastName;
+    }
+    
+    public void setPodcastName(String podcastName) {
+        this.podcastName = podcastName;
+    }
+    
+}
