cf764571098edbceec973174f7b3a2b3477f5d93
otsepp <tseppanen@protonmail.com>
2017-11-25 21:37:04 +0200

    test fix

--- src/test/java/ohtu/domain/BookTest.java
+++ src/test/java/ohtu/domain/BookTest.java
@@ -24,13 +24,13 @@ public class BookTest {
     
     @Before
     public void setUp() {
-        b = new Book("Jorma", "Lama 101", "Tosi jees", "978-951-98548-9-2");
+        b = new Book("Lama 101", "Jorma", "Tosi jees", "978-951-98548-9-2");
     }
     
     @Test
     public void bookConstructorWorks() {
-        assertEquals("Jorma", b.getCreator());
         assertEquals("Lama 101", b.getTitle());
+        assertEquals("Jorma", b.getCreator());
         assertEquals("Tosi jees", b.getDescription());
         assertEquals("978-951-98548-9-2", b.getISBN());
     }
